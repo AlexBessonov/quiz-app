56c9625615c2dbb87ce9f576ef76bfac1a34bef6
Alexander Bessonov <xander.bessonov@yandex.ru>
2020-04-12 00:30:46 +0300

    Сделана валидация формы

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout"
+import Quiz from "./containers/Quiz/Quiz"
+import {Route, Switch} from 'react-router-dom'
+import QuizList from "./containers/QuizList/QuizList"
+import QiuzCreator from "./containers/QiuzCreator/QiuzCreator"
+import Auth from "./containers/Auth/Auth"
 
 function App() {
   return (
     <Layout>
-
-
-      <div style={{width: 400, border: '1px solid black'}}>
-        <h1>Layout works</h1>
-      </div>
-
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/quiz-creator' component={QiuzCreator} />
+        <Route path='/quiz/:id' component={Quiz} />
+        <Route path='/' component={QuizList} />
+      </Switch>
     </Layout>
   );
 }
---
+++ src/containers/QiuzCreator/QiuzCreator.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class QiuzCreator extends Component {
+    render() {
+        return (
+            <div>
+                <h1>QiuzCreator</h1>
+            </div>
+        )
+    }
+}
